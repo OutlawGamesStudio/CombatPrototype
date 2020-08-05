2a0981912e532f18c7fded2c9a537eb9a722f64f
Sean McElholm <seanmcelholm@gmail.com>
2020-08-05 13:41:07 +0100

    Added AiPatrol state.

---
+++ Assets/Scripts/AI/States/AiPatrol.cs
@@ -0,0 +1,60 @@
+﻿using ForgottenLegends.Core;
+using UnityEngine;
+
+namespace ForgottenLegends.AI
+{
+    [CreateAssetMenu(fileName = "PatrolState", menuName = "AI/States/Patrol")]
+    public class AiPatrol : AiState
+    {
+        public float destinationTolerance = 1f;
+        private PatrolNode m_StartingNode;
+        private PatrolNode m_CurrentNode;
+
+        public override bool EnterState(StateMachine stateMachine, float deltaTime)
+        {
+            PatrolNode[] patrolNodes = GameObject.FindObjectsOfType<PatrolNode>();
+            float minDistance = 25f;
+            PatrolNode nearestNode = null;
+            foreach(var patrolNode in patrolNodes)
+            {
+                float dist = Vector3.Distance(stateMachine.transform.position, patrolNode.transform.position);
+                if (dist < minDistance)
+                {
+                    minDistance = dist;
+                    nearestNode = patrolNode;
+                }
+            }
+            if(nearestNode != null)
+            {
+                m_StartingNode = nearestNode;
+            }
+            if(m_StartingNode == null)
+            {
+                UnityEngine.Debug.LogError($"No starting node found.");
+                return false;
+            }
+            m_CurrentNode = m_StartingNode;
+            return base.EnterState(stateMachine, deltaTime);
+        }
+
+        public override void Execute(float deltaTime)
+        {
+            float distance = Vector3.Distance(m_StateMachine.transform.position, m_CurrentNode.transform.position);
+            if (distance <= destinationTolerance)
+            {
+                m_StateMachine.StopPathing();
+                if(m_CurrentNode.LinkedNode != null)
+                {
+                    m_CurrentNode = m_CurrentNode.LinkedNode;
+                    return;
+                }
+                if(shouldRepeat == true)
+                {
+                    m_CurrentNode = m_StartingNode;
+                }
+                return;
+            }
+            m_StateMachine.PathTo(m_CurrentNode.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Core/PatrolNode.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace ForgottenLegends.Core
+{
+    public class PatrolNode : MonoBehaviour
+    {
+        public PatrolNode LinkedNode => m_LinkedNode;
+        [SerializeField] private PatrolNode m_LinkedNode = null;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(transform.position, 0.5f);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (m_LinkedNode != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, m_LinkedNode.transform.position);
+            }
+        }
+    }
+}
